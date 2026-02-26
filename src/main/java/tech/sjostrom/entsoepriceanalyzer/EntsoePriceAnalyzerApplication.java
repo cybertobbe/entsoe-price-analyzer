049c2c2f5b3efd859fddfc799c967d1d5040cf90
cybertobbe <tobias.sjostrom@iths.se>
2026-02-26 20:05:27 +0100

    Initial commit

---
+++ src/main/java/tech/sjostrom/entsoepriceanalyzer/EntsoePriceAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package tech.sjostrom.entsoepriceanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntsoePriceAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntsoePriceAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sjostrom/entsoepriceanalyzer/EntsoePriceAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sjostrom.entsoepriceanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntsoePriceAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
